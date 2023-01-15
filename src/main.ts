e6aaedccfd48972644089f241534eabe4a1db6ec
zh <zhxhyy@qq.com>
2023-01-15 16:31:40 +0800

    dpplayer

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,26 @@
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
+import thumbnails from './thumbnails.jpg'
+
+import DPlayer from 'dplayer'
+
+document.getElementById('video_uploader')?.addEventListener('change', (e: Event) => {
+  // @ts-ignore
+  const file = e.target.files[0]
+
+  // @ts-ignore
+  const blobURL: string = URL.createObjectURL(file)
+
+  console.log(blobURL)
+
+  const dp = new DPlayer({
+    container: document.getElementById('dplayer'),
+    screenshot: true,
+    video: {
+      url: blobURL,
+      thumbnails,
+    },
+  })
+
+  dp.play()
+})
